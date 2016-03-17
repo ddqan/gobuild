29237025f4ae37303465619a35b970bf7e3b50ab
An Dang <an.dang@lazada.com>
2016-03-17 21:32:30 +0700

    add build file

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+
+doCommand() {
+    typeset cmnd="$*"
+    check=$($cmnd)
+
+    if [[ $? != 0 ]]; then
+        echo "$(tput setaf 1)Build Failed"
+        exit 1
+    elif [[ $check != "" ]]; then
+        echo "$(tput setaf 1)$check"
+        exit 1
+    fi
+}
+
+isInstall=$1
+
+cmd="goimports -w ./"
+doCommand $cmd
+
+cmd="golint ./..."
+doCommand $cmd
+
+cmd="gofmt -w ./"
+doCommand $cmd
+
+cmd="go build"
+doCommand $cmd
+
+successMsg="$(tput setaf 2)Done. All your golang code are re-built. CLI Example:"
+if [[ ${isInstall} == "install" ]]; then
+    cmd="go install"
+    doCommand $cmd
+    successMsg="$(tput setaf 2)Done. All your golang code are re-built and installed. CLI Example:"
+fi
+
+echo "$successMsg"
